633033669608baf79cd612bd02346c42addb854b
yesaroun <akor1@naver.com>
2024-05-03 23:55:55 +0900

    [feat] 검색 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const [pokemons, setPokemons] = useState([]);
   const [offset, setOffset] = useState(0);
   const [limit, setLimit] = useState(20);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetchPokeData(true);
@@ -25,10 +26,48 @@ function App() {
     }
   };
 
+  const handleSearchInput = async (e) => {
+    setSearchTerm(e.target.value);
+    if (e.target.value.length > 0) {
+      try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${e.target.value}`);
+        const poketmonData = {
+          url: `https://pokeapi.co/api/v2/pokemon/${response.data.id}`,
+          name: searchTerm,
+        };
+        setPokemons([poketmonData]);
+      } catch (error) {
+        setPokemons([]);
+        console.error(error);
+      }
+    } else {
+      fetchPokeData(true);
+    }
+  };
+
   return (
     <article className="pt-6">
       <header className="flex flex-col gap-2 w-full px-4 z-50">
-        {/* Input Form 부분 */}
+        <div className="relative z-50">
+          <form
+            className="relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto"
+          >
+            <input
+              onChange={handleSearchInput}
+              value={searchTerm}
+              type="text"
+              className="text-xs w-[20.5rem] h-6 px-2 py-1 rounded-lg bg-[hsl(214,13%,47%)] text-gray-300 text-center"
+              placeholder=" "
+            />
+            <button
+              type="submit"
+              className="fa fa-2x text-xs bg-slate-900 text-slate-300 w-[2.5rem] h-6 px-2 py-1 rounded-r-lg
+              text-center absolute right-0 hover:bg-slate-700 z-0"
+            >
+              검색
+            </button>
+          </form>
+        </div>
       </header>
       <section className="pt-6 flex flex-col justify-content items-center overflow-auto z-0">
         <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
