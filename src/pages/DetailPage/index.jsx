e9dfa923152b4cc5945d78802c39b9ed2ad0cffc
yesaroun <akor1@naver.com>
2024-05-08 01:47:24 +0900

    [feat] 포켓몬 상세 페이지 UI 구현

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,6 +1,13 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Loading } from "../../assets/Loading.jsx";
+import { LessThan } from "../../assets/LessThan.jsx";
+import { ArrowLeft } from "../../assets/ArrowLeft.jsx";
+import { GreaterThan } from "../../assets/GreaterThan.jsx";
+import { Balance } from "../../assets/Balance.jsx";
+import { Vector } from "../../assets/Vector.jsx";
+import Type from "../../components/Type.jsx";
 
 const DetailPage = () => {
   const [pokemon, setPokemon] = useState();
@@ -38,6 +45,7 @@ const DetailPage = () => {
         const formattedPokemonData = {
           id,
           name,
+          types: types.map(type => type.type.name),
           weight: weight / 10,
           height: height / 10,
           previous: nextAndPreviousPokemon.previous,
@@ -50,6 +58,7 @@ const DetailPage = () => {
       }
     } catch (error) {
       console.error(error);
+      setIsLoading(false);
     }
   }
 
@@ -96,11 +105,126 @@ const DetailPage = () => {
     };
   }
 
-  if (isLoading) return <div>...loading</div>;
+  if (isLoading) return (
+    <div className={`absolute h-auto w-auto top-1/3 -translate-x-1/2 left-1/2 z-50`}>
+      <Loading className="w-12 h-12 z-50 animate-spin text-slate-900" />
+    </div>
+  );
+
+  if (!isLoading && !pokemon) {
+    return (<div>Not Found...</div>);
+  }
+
+  const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`;
+  const bg = `bg-${pokemon?.types?.[0]}`;
+  const text = `text-${pokemon?.types?.[0]}`;
 
   return (
-    <div>DetailPage</div>
-  );
+    <article className="flex return items-center gap-1 flex-col w-full">
+      <div
+        className={`${bg} w-auto h-full flex flex-col z-0 items-center justify-end relative overflow-hidden`}
+      >
+
+        {pokemon.previous && (
+          <Link
+            className="absolute top-[40%] -translate-y-1/2 z-50 left-1"
+            aria-label={`Previous pokemon`}
+            to={`/pokemon/${pokemon.previous}`}
+          >
+            <LessThan className="w-5 h-8 p-1" />
+          </Link>
+        )}
+        {pokemon.next && (
+          <Link
+            className="absolute top-[40%] -translate-y-1/2 z-50 right-1"
+            aria-label={`Next pokemon`}
+            to={`/pokemon/${pokemon.next}`}
+          >
+            <GreaterThan className="w-5 h-8 p-1" />
+          </Link>
+        )}
+
+        <section className="w-full flex flex-col z-20 items-center justify-end relative h-full">
+          <div className="absolute z-30 top-6 flex items-center w-full justify-between px-2">
+            <div className="flex items-center gap-1">
+              <Link aria-label="Go to Pokedex" to="/">
+                <ArrowLeft className="w-6 h-8 text-zinc-200" />
+              </Link>
+              <h1 className="text-zinc-200 font-bold text-xl capitalize">
+                {pokemon.name.replaceAll("-", " ")}
+              </h1>
+            </div>
+            <div className="text-zinc-200 font-bold text-md">
+              #{pokemon.id.toString().padStart(3, "00")}
+            </div>
+          </div>
+          <div className="relative h-auto max-w-[15.5rem] z-20 mt-6 -mb-16">
+            <img
+              src={img}
+              width="100%"
+              height="auto"
+              loading="lazy"
+              alt={pokemon.name.replaceAll("-", " ")}
+              className={`object-contain h-full`}
+            />
+          </div>
+        </section>
+        <section
+          className="w-full min-h-[65%] h-full bg-gray-800 z-10 pt-14 flex flex-col items-center gap-3 px-5 pb-4">
+          <div className="flex items-center justify-center gap-4">
+            {/* 포켓몬 타입 */}
+            {pokemon.types.map((type) => (
+              <Type key={type} type={type} />
+            ))}
+          </div>
+
+          <h2 className={`text-base font-semibold ${text}`}>
+            정보
+          </h2>
+
+          <div className="flex w-full items-center justify-between max-w-[400px] text-center">
+            <div className="w-full">
+              <h4 className="text-[0.5rem] text-zinc-100">Weight</h4>
+              <div className="text-sm flex mt-1 gap-2 justify-center text-zinc-200">
+                <Balance />
+                {pokemon.weight}kg
+              </div>
+            </div>
+            <div className="w-full">
+              <h4 className="text-[0.5rem] text-zinc-100">Height</h4>
+              <div className="text-sm flex mt-1 gap-2 justify-center text-zinc-200">
+                <Vector />
+                {pokemon.height}m
+              </div>
+            </div>
+            <div className="w-full">
+              <h4 className="text-[0.5rem] text-zinc-100">Moves</h4>
+              {pokemon.abilities.map((e, i) => (
+                <div key={e} className="text-[0.5rem] text-zinc-100 capitalize">{e}</div>
+              ))}
+            </div>
+          </div>
+
+          <h2 className={`text-base font-semibold ${text}`}>
+            기본 능력치
+          </h2>
+          <div className="w-full">
+            {/* Stats */}
+          </div>
+
+          {pokemon.DamageRelations && (
+            <div className="w-10/12">
+              <h2 className={`capitalize font-semibold text-base ${text} text-center`}>
+                Type effectiveness
+              </h2>
+              {/* Damage */}
+            </div>
+          )}
+        </section>
+      </div>
+    </article>
+  )
+    ;
 };
 
 export default DetailPage;
