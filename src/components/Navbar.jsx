ae9c8f1bdfe0ef0b0abcef47ea2f8c4163511a86
yesaroun <akor1@naver.com>
2024-05-16 02:40:35 +0900

    [feat] navbar ui 구현

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,83 @@
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+
+const NavWrapper = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 99999;
+  background-color: ${props => props.show ? "#090b13" : "transparent"};
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 50px;
+  margin-top: 4px;
+
+  img {
+    width: 100%;
+  }
+`;
+
+const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`;
+
 const Navbar = () => {
 
+  const [show, setShow] = useState(false);
+
+  const listener = () => {
+    if (window.scrollY > 50) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listener);
+
+    return () => {
+      window.removeEventListener("scroll", listener);
+    };
+  }, []);
+
+  return (
+    <NavWrapper show={show}>
+      <Logo>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
+          alt="Poke Logo"
+          onClick={() => (window.location.href = "/")}
+        />
+      </Logo>
+      {/*{pathname === "/login" ? (*/}
+      {/*    <Login onClick={handleAuth}>Login</Login>*/}
+      {/*  ) :*/}
+      {/*  <></>*/}
+      {/*}*/}
+    </NavWrapper>
+  );
 };
 
 export default Navbar;
