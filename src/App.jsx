d9387357a578611d9cc59e82d7af6a23de752dcf
yesaroun <akor1@naver.com>
2024-05-15 23:11:50 +0900

    [feat] 로그인 페이지 경로 생성 및 Navbar 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage/index.jsx";
 import DetailPage from "./pages/DetailPage/index.jsx";
+import LoginPage from "./pages/LoginPage/index.jsx";
+import Navbar from "./components/Navbar.jsx";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <br />
+      <br />
+      <br />
+      <Outlet />
+    </>
+  );
+};
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/pokemon/:id" element={<DetailPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="pokemon/:id" element={<DetailPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
